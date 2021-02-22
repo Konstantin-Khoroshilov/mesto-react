162137639b157872b4b287bef1e8b841706fd7f3
Konstantin Khoroshilov <manly41142009@yandex.ru>
2021-02-22 17:08:59 +0300

    Updates

---
+++ precache-manifest.d5e3c634116f56f56ebdf37e790ec590.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd1555e395d09fb45c219e7f8d816e3",
+    "url": "/Konstantin-Khoroshilov/mesto-react/index.html"
+  },
+  {
+    "revision": "cea9f85d6cc16f878e74",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/css/main.ce7cfa3d.chunk.css"
+  },
+  {
+    "revision": "8fe3a0a3bfd4d25aafdc",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/js/2.76d7b76d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/js/2.76d7b76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea9f85d6cc16f878e74",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/js/main.aca0eb5c.chunk.js"
+  },
+  {
+    "revision": "255cbf6d75de15f10d50",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/js/runtime-main.9792c601.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/add-button.bdfb8ebb.svg"
+  },
+  {
+    "revision": "c7df05485663b2b3ec3b99326f0f5ea2",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/close-icon.c7df0548.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/delete-button.efa6fb04.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/edit.58afbe73.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/like-button-active.9fdebb6b.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/like-button.6c74d9b5.svg"
+  },
+  {
+    "revision": "bb4a1e8606f5de17d05539cb6dfd9b6c",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/load-error.bb4a1e86.gif"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/Konstantin-Khoroshilov/mesto-react/static/media/logo.855a8c98.svg"
+  }
+]);
